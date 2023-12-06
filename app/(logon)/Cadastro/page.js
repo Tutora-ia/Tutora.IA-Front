497d599fb6a7830bca9d80c381d004e1f5df704e
DaviRSouza <144457305+DaviRSouza@users.noreply.github.com>
2023-12-06 06:17:50 -0300

    Pequeno ajuste no backgroundLogin do cadastro

--- app/(logon)/Cadastro/page.js
+++ app/(logon)/Cadastro/page.js
@@ -8,82 +8,77 @@ import BackgroundLogin from '@/app/components/backgroundWithPurpleCircles.compon
 export default function Cadastro() {
 
     return (
-        <BackgroundLogin
-
-            children={
-                <>
-                    <div className={styles.ArrowBackButton}>
-                        <ArrowBackButton />
+        <BackgroundLogin>
+            <>
+                <div className={styles.ArrowBackButton}>
+                    <ArrowBackButton />
+                </div>
+                <div className={styles.main}>
+                    <div className={styles.textTop}>
+                        <h1>Seja bem-vindo!</h1>
+                        <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
                     </div>
-                    <div className={styles.main}>
-                        <div className={styles.textTop}>
-                            <h1>Seja bem-vindo!</h1>
-                            <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
-                        </div>
 
-                        <div className={styles.container}>
+                    <div className={styles.container}>
 
-                            <form className={styles.form} action="">
-                                <h1>Cadastre-se</h1>
+                        <form className={styles.form} action="">
+                            <h1>Cadastre-se</h1>
 
-                                <div className={styles.icon}>
-                                    <Image
-                                        className={styles.googleIcon}
-                                        src={GoogleIcon}
-                                        alt=''
-                                        width={45}
-                                        height={45}
-                                    />
+                            <div className={styles.icon}>
+                                <Image
+                                    className={styles.googleIcon}
+                                    src={GoogleIcon}
+                                    alt=''
+                                    width={45}
+                                    height={45}
+                                />
+                            </div>
+
+
+                            <div className={styles.otherOptions}>
+                                <div className={styles.line}>
+                                    <hr className={styles.hrEstilizada} />
                                 </div>
-
-
-                                <div className={styles.otherOptions}>
-                                    <div className={styles.line}>
-                                        <hr className={styles.hrEstilizada} />
-                                    </div>
-                                    <p className={styles.ou}>OU</p>
-                                    <div className={styles.line}>
-                                        <hr className={styles.hrEstilizada} />
-                                    </div>
+                                <p className={styles.ou}>OU</p>
+                                <div className={styles.line}>
+                                    <hr className={styles.hrEstilizada} />
                                 </div>
+                            </div>
 
-                                <div className={styles.inputbox}>
-                                    <input type="text" placeholder="Nome" required />
-                                </div>
+                            <div className={styles.inputbox}>
+                                <input type="text" placeholder="Nome" required />
+                            </div>
 
-                                <div className={styles.inputbox}>
-                                    <input type="text" placeholder="E-mail" required />
+                            <div className={styles.inputbox}>
+                                <input type="text" placeholder="E-mail" required />
 
-                                </div>
-                                <div className={styles.inputbox}>
-                                    <input type="password" placeholder="Senha" required />
-                                </div>
+                            </div>
+                            <div className={styles.inputbox}>
+                                <input type="password" placeholder="Senha" required />
+                            </div>
 
-                                <button type="submit" className={styles.btn}>
-                                    Cadastrar
-                                </button>
-                            </form>
-                        </div>
-
-                        <div className={styles.textright}>
-                            <h1>Seja bem-vindo!</h1>
-
-                            <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
-
-                            <h3>Já tem uma conta?</h3>
-
-                            <button type="button" className={styles.button}>
-                                <Link href={'/login'}>
-                                    Login
-                                </Link>
+                            <button type="submit" className={styles.btn}>
+                                Cadastrar
                             </button>
-
-                        </div>
+                        </form>
                     </div>
-                </>
 
-            }
+                    <div className={styles.textright}>
+                        <h1>Seja bem-vindo!</h1>
 
-        />
+                        <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
+
+                        <h3>Já tem uma conta?</h3>
+
+                        <button type="button" className={styles.button}>
+                            <Link href={'/login'}>
+                                Login
+                            </Link>
+                        </button>
+
+                    </div>
+                </div>
+            </>
+        </BackgroundLogin>
     )
 }
