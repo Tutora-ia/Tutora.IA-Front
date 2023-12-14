49b54dd1043674f227b6214cf7578c2c366b5fe8
DaviRSouza <144457305+DaviRSouza@users.noreply.github.com>
2023-12-14 11:29:33 -0300

    Cadastro Funcionando

--- app/(logon)/cadastro/page.js
+++ app/(logon)/cadastro/page.js
@@ -18,15 +18,16 @@ export default function Cadastro() {
     } = useForm();
 
     const onSubmit = async (data) => {
+        console.log(JSON.stringify(data, null, 2));
         try {
-            const response = await fetch("http://localhost:8080/v1/auth/cadastro", {
+            const response = await fetch("http://localhost:8080/v1/auth/sign-up", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify(data)
             });
-    
+
             if (response.ok) {
                 alert("Cadastro realizado com sucesso!");
             } else {
@@ -36,94 +37,94 @@ export default function Cadastro() {
             console.error(error);
         }
     };
-    
-return (
-    <BackgroundLogin heightContainer='100vh' circleColor='#6C33D7'>
-        <>
-            <div className={styles.ArrowBackButton}>
-                <ArrowBackButton />
-            </div>
-            <div className={styles.main}>
-                <div className={styles.textTop}>
-                    <h1>Seja bem-vindo!</h1>
-                    <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
+
+    return (
+        <BackgroundLogin heightContainer='100vh' circleColor='#6C33D7'>
+            <>
+                <div className={styles.ArrowBackButton}>
+                    <ArrowBackButton />
                 </div>
+                <div className={styles.main}>
+                    <div className={styles.textTop}>
+                        <h1>Seja bem-vindo!</h1>
+                        <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
+                    </div>
 
-                <div className={styles.container}>
+                    <div className={styles.container}>
 
-                    <div className={styles.form} action="">
-                        <h1>Cadastre-se</h1>
+                        <div className={styles.form} action="">
+                            <h1>Cadastre-se</h1>
 
-                        <div className={styles.icon}>
-                            <Image
-                                className={styles.googleIcon}
-                                src={GoogleIcon}
-                                alt=''
-                                width={45}
-                                height={45}
-                            />
-                        </div>
-
-
-                        <div className={styles.otherOptions}>
-                            <div className={styles.line}>
-                                <hr className={styles.hrEstilizada} />
+                            <div className={styles.icon}>
+                                <Image
+                                    className={styles.googleIcon}
+                                    src={GoogleIcon}
+                                    alt=''
+                                    width={45}
+                                    height={45}
+                                />
                             </div>
-                            <p className={styles.ou}>OU</p>
-                            <div className={styles.line}>
-                                <hr className={styles.hrEstilizada} />
+
+
+                            <div className={styles.otherOptions}>
+                                <div className={styles.line}>
+                                    <hr className={styles.hrEstilizada} />
+                                </div>
+                                <p className={styles.ou}>OU</p>
+                                <div className={styles.line}>
+                                    <hr className={styles.hrEstilizada} />
+                                </div>
                             </div>
-                        </div>
 
-                        <div className={styles.inputbox}>
-                            <input
-                                className={errors?.name && styles.inputError}
-                                type="text"
-                                placeholder="Nome"
-                                {...register("nome", { required: true })}
-                            />
-                        </div>
+                            <div className={styles.inputbox}>
+                                <input
+                                    className={errors?.name && styles.inputError}
+                                    type="text"
+                                    placeholder="Nome"
+                                    {...register("name", { required: true })}
+                                />
+                            </div>
 
-                        <div className={styles.inputbox}>
-                            <input
-                                className={errors?.name && styles.inputError}
-                                type="text"
-                                placeholder="E-mail"
-                                {...register("email", { required: true, validate: (value) => isEmail(value), })}
-                            />
+                            <div className={styles.inputbox}>
+                                <input
+                                    className={errors?.name && styles.inputError}
+                                    type="text"
+                                    placeholder="E-mail"
+                                    {...register("email", { required: true, validate: (value) => isEmail(value), })}
+                                />
+                            </div>
+                            <div className={styles.inputbox}>
+                                <input
+                                    className={errors?.name && styles.inputError}
+                                    type="password"
+                                    placeholder="Senha"
+                                    {...register("password", { required: true, minLength: 4 })}
+                                />
+                                {errors?.password?.type === "minLength" && (
+                                    <p className={styles.error}> A senha deve ter no minimo 4 caracteres </p>
+                                )}
+                            </div>
+                            <button onClick={() => handleSubmit(onSubmit)()} className={styles.btn}>
+                                Cadastrar
+                            </button>
                         </div>
-                        <div className={styles.inputbox}>
-                            <input
-                                className={errors?.name && styles.inputError}
-                                type="password"
-                                placeholder="Senha"
-                                {...register("senha", { required: true, minLength: 4 })}
-                            />
-                            {errors?.password?.type === "minLength" && (
-                                <p className={styles.error}> A senha deve ter no minimo 4 caracteres </p>
-                            )}
-                        </div>
-                        <button onClick={() => handleSubmit(onSubmit)()} className={styles.btn}>
-                            Cadastrar
+                    </div>
+
+                    <div className={styles.textright}>
+                        <h1>Seja bem-vindo!</h1>
+
+                        <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
+
+                        <h3>Já tem uma conta?</h3>
+
+                        <button type="button" className={styles.button}>
+                            <Link href={'/login'}>
+                                Login
+                            </Link>
                         </button>
                     </div>
                 </div>
-
-                <div className={styles.textright}>
-                    <h1>Seja bem-vindo!</h1>
-
-                    <h2>Preparado para dar um play na sua<br />evolução gramatical?</h2>
-
-                    <h3>Já tem uma conta?</h3>
-
-                    <button type="button" className={styles.button}>
-                        <Link href={'/login'}>
-                            Login
-                        </Link>
-                    </button>
-                </div>
-            </div>
-        </>
-    </BackgroundLogin>
-)
+            </>
+        </BackgroundLogin>
+    )
 }
